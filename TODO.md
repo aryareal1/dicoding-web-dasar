9301898f305be61ede431f3c8b0e50d1c3be0ef7
aryareal <aryadwiputra193@gmail.com>
2026-01-01 01:24:40 +0700

    feat: add main content & aside

--- TODO.md
+++ TODO.md
@@ -4,21 +4,21 @@
 - [x] Use `<header>`.
 - [x] Use `<nav>`.
 - [x] Use `<footer>`.
-- [ ] Use `<main>`.
-- [ ] Use `<article>`.
-- [ ] Use `<aside>`.
+- [x] Use `<main>`.
+- [x] Use `<article>`.
+- [x] Use `<aside>`.
 - [x] Add `<a>` link to my dicoding profile in `<nav>`.
-- [ ] Add image in `<aside>`.
-- [ ] Use **flexbox**.
+- [x] Add image in `<aside>`.
+- [x] Use **flexbox**.
 
 ## Tips
-- [ ] Use **relative pathing**.
-- [ ] Refer to external files correctly.
+- [x] Use **relative pathing**.
+- [x] Refer to external files correctly.
 - [x] Use **IDE** (VSCode).
 
 ## Suggestions
 - [x] Separate styling as external `.css` file.
-- [ ] Add `alt` into images.
-- [ ] Use **heading** correctly.
-- [ ] Use `@media screen`.
+- [x] Add `alt` into images.
+- [x] Use **heading** correctly (`<h1>` must be used once).
+- [x] Use `@media screen`.
 - [x] Add little bit of `.js`.
--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+// Elements declaration
 const _html = document.querySelector("html");
 /** @type {HTMLDivElement} */
 const _jumbotron = document.querySelector(".jumbotron");
 /** @type {HTMLDivElement} */
 const _navTitle = document.querySelector("nav .title");
 
+/**
+ * Handle page scrolling.
+ * @remarks Toggle the visibility of title in `nav`.
+ */
 function onScroll() {
-  if (_html.scrollTop > _jumbotron.scrollHeight)
-    _navTitle.style.animation = "0.75s ease forwards title-in";
-  else if (_html.scrollTop < _jumbotron.scrollHeight / 2)
-    _navTitle.style.animation = "0.75s ease forwards title-out";
+  if (_html.scrollTop > _jumbotron.scrollHeight) {
+    _navTitle.style.animation = "0.75s ease title-in";
+    _navTitle.style.opacity = 1;
+  } else if (_html.scrollTop < _jumbotron.scrollHeight / 2) {
+    _navTitle.style.animation = null;
+    _navTitle.style.opacity = 0;
+  }
 }
